5190c17f7601972eb460e8e0db01dd836ad248d5
Impossible777 <cristmichael7@gmail.com>
2023-12-22 17:44:59 -0600

    Worked on function to pull addresses

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import csv
 
 # Path to the packageInfo.csv file
 packageInfoPath = 'packageInfo.csv'
+distanceTablePath = 'WGUPS Distance Table (1).csv'
 
 # Creating the hash table
 packageInfo = createHashMap()
@@ -23,3 +24,52 @@ with open(packageInfoPath, 'r', encoding='utf-8-sig') as csv_file:
 truck1 = truck(16, 18)
 truck2 = truck(16, 18)
 truck3 = truck(16, 18)
+
+def normalize_address(address):
+    return ' '.join(address.split())
+
+def getAddresses(address1, address2):
+    
+    
+    # This function takes two addresses and returns the distance between them in miles
+    with open(distanceTablePath, 'r', encoding='utf-8-sig') as distanceCSV:
+        distanceReader = csv.reader(distanceCSV)
+        for row in distanceReader:
+            if row:
+                normalized_address = normalize_address(row[0])
+                if address1 == normalized_address:
+                    addressOneIndex = row.index(address1)
+                    break
+        header = next(distanceReader, None)
+
+        if header:
+            for col_index, value in enumerate(header):
+                normalized_address = normalize_address(value)
+                if address2 == normalized_address:
+                    addressTwoIndex = col_index
+                    break
+        
+        return addressOneIndex, addressTwoIndex
+    
+def getCellValue(distanceTablePath, row_index, col_index):
+    with open(distanceTablePath, 'r', encoding='utf-8-sig') as distanceCSV:
+        distanceReader = csv.reader(distanceCSV)
+        
+        for _ in range(row_index + 1):
+            row = next(distanceReader, None)
+            if row is None:
+                return None
+            
+        if row is not None and col_index < len(row):
+            return row[col_index]
+        else: return None
+
+
+addressOne = "International Peace Gardens 1060 Dalton Ave S"
+addressTwo = "Sugar House Park 1330 2100 S"
+addressOneIndex, addressTwoIndex = getAddresses(addressOne, addressTwo)
+print(getCellValue(distanceTablePath, addressOneIndex, addressTwoIndex))
+            
+
+    
+
--- truck.py
+++ truck.py
@@ -2,12 +2,17 @@ class truck:
     def __init__(self, maxPackages, avgSpeed, packages = [], currentLocation = 'HUB'):
         self.maxPackages = maxPackages
         self.avgSpeed = avgSpeed
+        self.packages = packages
+        self.currentLocation = currentLocation
     
     def __str__(self):
         return f"This truck is currently at {self.currentLocation} and has {self.packages} on board."
     
     def addPackages(self, package):
-        self.packages.append(package)
+        if len(self.packages) >= self.maxPackages:
+            print("This truck is full.")
+        else:
+            self.packages.append(package)
     
     def removePackages(self, package):
         self.packages.remove(package)
